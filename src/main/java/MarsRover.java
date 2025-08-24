a3ae4803c79af0f486214249f98aca4483f8fc85
KieranGu <kieran.gu@oocl.com>
2025-08-24 16:20:00 +0800

    refactor:用列表管理方向，简化转向操作代码

--- src/main/java/MarsRover.java
+++ src/main/java/MarsRover.java
@@ -1,3 +1,6 @@
+import java.util.Arrays;
+import java.util.List;
+
 public class MarsRover {
     private int x;
     private int y;
@@ -54,29 +57,19 @@ public class MarsRover {
             x--;
         }
     }
-    private void turnLeft()
-    {
-        if(direction.equals(Direction.N)) {
-            direction = Direction.W;
-        } else if(direction.equals(Direction.E)) {
-            direction = Direction.N;
-        } else if(direction.equals(Direction.S)) {
-            direction = Direction.E;
-        } else if(direction.equals(Direction.W)) {
-            direction = Direction.S;
-        }
+
+    private static final List<Direction> DIRECTIONS = Arrays.asList(
+            Direction.N, Direction.E, Direction.S, Direction.W
+    );
+    private void turnLeft() {
+        int currentIndex = DIRECTIONS.indexOf(direction);
+        int newIndex = (currentIndex + 3) % DIRECTIONS.size(); // +3来往前找
+        direction = DIRECTIONS.get(newIndex);
     }
-    private void turnRight()
-    {
-        if(direction.equals(Direction.N)) {
-            direction = Direction.E;
-        } else if(direction.equals(Direction.E)) {
-            direction = Direction.S;
-        } else if(direction.equals(Direction.S)) {
-            direction = Direction.W;
-        } else if(direction.equals(Direction.W)) {
-            direction = Direction.N;
-        }
+    private void turnRight() {
+        int currentIndex = DIRECTIONS.indexOf(direction);
+        int newIndex = (currentIndex + 1) % DIRECTIONS.size();
+        direction = DIRECTIONS.get(newIndex);
     }
 
 
