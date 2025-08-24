2e06c5466d083f2344ef1948302e31fb7f85f389
KieranGu <kieran.gu@oocl.com>
2025-08-24 15:38:51 +0800

    feat: 编写实现了针对第一个test case的功能

--- src/main/java/MarsRover.java
+++ src/main/java/MarsRover.java
@@ -4,13 +4,30 @@ public class MarsRover {
     private Direction direction;
 
     public MarsRover(int x, int y, Direction direction) {
-
+        this.x = x;
+        this.y = y;
+        this.direction = direction;
     }
 
     public void execute(String command) {
+        if (command.equals("M")) {
+            moveForward();
+        }
+    }
+
+    private void moveForward() {
+        if (direction.equals(Direction.N)) {
+            y++;
+        } else if (direction.equals(Direction.E)) {
+            x++;
+        } else if (direction.equals(Direction.S)) {
+            y--;
+        } else if (direction.equals(Direction.W)) {
+            x--;
+        }
     }
 
     public String report() {
-        return "(0, 1) N";
+        return "(" + x + ", " + y + ") " + direction.toString();
     }
 }
